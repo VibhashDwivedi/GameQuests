084fee12387af166150eb1b9b7b48d13cab603cb
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-08-13 01:16:49 +0530

    filters

--- Section1/frontend/src/components/Home.jsx
+++ Section1/frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from './Navbar'
 import LandingPage from './LandingPage';
 import useUserContext from '../UserContext';
@@ -6,7 +6,11 @@ import useUserContext from '../UserContext';
 const Home = () => {
 
 
-    const date = new Date();
+    const current = new Date();
+    const date = current.getDate();
+   // const checkbox = useRef();
+   console.log(date)
+    const [check, setcheck] = useState(false)
     const [Tlist, setTlist] = useState([]);
     const [search, setsearch] = useState([])
 
@@ -35,6 +39,7 @@ const Home = () => {
     const displayTournament = () => {
         if(Tlist.length === 0) return <h1 className='text-center '>No Data Found</h1>
 
+        
         return Tlist.map((tournament) => (<><div className='col-md-3 mb-4 '>
             <div className='card tournament-card mx-2 mt-3' >
             <img width={212} height={150} className='mx-3 my-2 rounded-2' src={"http://localhost:5000/"+tournament.image} alt="" />
@@ -60,6 +65,35 @@ const Home = () => {
         ))
     }
 
+
+
+    const handleChange =(e)=>{
+        setcheck(!check);
+    }
+
+    const filterTournamentPlatform = (e) =>{
+        //const value = e.target.value;
+        setTlist(search.filter((t) =>{
+            if(!check) 
+            return t.platform==='PC'
+        else 
+        return t.platform==='PC'||t.platform==='Mobile'
+        
+     } )
+        );
+    }
+    const filterTournamentPlatform2 = (e) =>{
+       // const value = e.target.value;
+        setTlist(search.filter((t) =>{
+            if(!check) 
+            return t.platform.toLowerCase()==='Mobile'.toLowerCase()
+        else 
+        return t.platform==='PC'||t.platform==='Mobile'
+        
+     } )
+        );
+    }
+
     
     const filterTournament = (e) =>{
         const value = e.target.value;
@@ -79,7 +113,16 @@ const Home = () => {
             <div className="container py-5 my-2 ">
                 <p className='display-2 text-center fw-bold text-white'>Search Tournaments</p>
                 <input type="text" className='form-control w-75 m-auto'  onChange={filterTournament} / >
-               
+                   
+                    <div className="container w-75 my-2"> <h5 htmlFor="" className='text-white'>Filters</h5>
+                    <div className='d-flex'>
+                    <button className="btn btn-warning rounded-5 "><input type="checkbox"  className='mx-1' onChange={filterTournamentPlatform} onClick={handleChange}/>PC</button>
+                    <button className="btn btn-warning rounded-5 mx-2"><input type="checkbox"  className='mx-1' onChange={filterTournamentPlatform2} onClick={handleChange}/>Mobile</button>
+                    
+                    </div>
+                    </div>
+                   
+             
             </div>
         </header>
         <div className='container'  >
